56c0d4c0e2a2787f5d65ea2fc4eeecb76f3cd40b
omar11 <omar.waller@ge.com>
2018-04-16 09:36:25 -0400

    Improved accuracy

--- decisiontreemodel.py
+++ decisiontreemodel.py
@@ -9,15 +9,23 @@ from sklearn.metrics import accuracy_score
 import scipy
 
 school_data = pandas.read_csv('frb_01_overall_new.csv')
-
+pandas.DataFrame({col: school_data[col].astype('category').cat.codes for col in school_data}, index=school_data.index)
 # X features are attributes that revealed a positive correlation with risk
 # Median Dependent Parent AGI R-Value = 0.39
 # % Borrowers without a Pell Grant R-Value = 0.47
 # % Dependent Borrower Count R-Value = 0.21
 # Mean Balance R-Value = 0.22
 
+#All numerical columns
+n=7
+cols = [n]
+for i in range(7,73):
+    cols.append(n+1)
+    n+=1
+    
+#X_features = school_data.iloc[:,[11,14,17,20]]
+X_features2 = school_data.iloc[:,cols]
 
-X_features = school_data.iloc[:,[11,14,17,20]]
 y_target = school_data.iloc[:,3]
 
 # Encode target classes to discrete values
@@ -31,11 +39,11 @@ print('Dictionary: '+ str(riskyness) + '\n')
 y_target = labelEncoder.fit_transform(y_target)
 
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X_features, y_target, 
+X_train, X_test, y_train, y_test = train_test_split(X_features2, y_target, 
                                                     test_size = 0.15, random_state = 0)
 
 #Train classifier with gini method
-classifier_gini = tree.DecisionTreeClassifier(criterion='gini', min_samples_split=220)
+classifier_gini = tree.DecisionTreeClassifier(criterion='gini', min_samples_split=320)
 
 #Fit training data to classifier
 classifier_gini = classifier_gini.fit(X_train,y_train)
@@ -44,7 +52,7 @@ classifier_gini = classifier_gini.fit(X_train,y_train)
 y_predict_gini = classifier_gini.predict(X_test)
 
 #Train classifier with entropy method
-classifier_entropy = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split=220)
+classifier_entropy = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split=320)
 classifier_entropy = classifier_gini.fit(X_train,y_train)
 
 #Predict with entropy method
