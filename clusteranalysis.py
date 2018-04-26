a2a38b9804e82756dd46e988aa37ab2108580868
omar11 <omar.waller@ge.com>
2018-04-26 16:47:00 -0400

    clusterrrr

--- clusteranalysis.py
+++ clusteranalysis.py
@@ -1,41 +1,57 @@
 # -*- coding: utf-8 -*-
 #%matplotlib inline
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()  # for plot styling
+import matplotlib.pyplot
+from matplotlib import style
+style.use('ggplot')
 import pandas
 import numpy 
-from sklearn import tree
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
-from sklearn.datasets.samples_generator import make_blobs
-
-n=7
-cols = [2,n]
-for i in range(7,73):
-    cols.append(n+1)
-    n+=1
-
-school_data = pandas.read_csv('frb_01_overall_new2.csv')
-X = school_data.iloc[:,cols].values
-#X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
-#plt.scatter(X[:, 0], X[:, 17], s=50)
+from sklearn.metrics import accuracy_score
+from matplotlib.pyplot import subplot
 
 
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
+dataset = pandas.read_csv('frb_01_overall_new2.csv')
+X = dataset.iloc[:,17:18].values  # % Borrowers without a Pell Grant
+y = dataset.iloc[:, 20].values  # Mean Balance
 
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
+Xy = numpy.array(list(zip(X,y)))
+kmeans = KMeans(n_clusters = 5)
+kmeans.fit(Xy)
+fig1 = matplotlib.pyplot.subplot()
 centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+labels = kmeans.labels_
+colors = ['r.','g.','b.','c.','k.','y.','m.']
+#matplotlib.pyplot.scatter(X,y)
+print('Displaying: % Borrowers without a Pell Grant vs. Mean Balance')
+fig1.scatter(centers[:, 0], centers[:, 1],zorder=1, c='black', s=100,alpha=0.85)
+for i in range(len(Xy)):
+    fig1.plot(Xy[i][0],Xy[i][1],
+                           colors[labels[i]],
+                           markersize=10)
+    
+fig1.set_title('% Borrowers without a Pell Grant vs. Mean Balance')
+fig1.set_xlabel('% Borrowers without a Pell Grant')
+fig1.set_ylabel('Mean Balance')
+fig1.show()
 
-
-
-
-
-
+#print('Displaying: Median Dependent Parent AGI vs. Mean Balance')
+#
+#X = dataset.iloc[:,14].values  # Median Dependent Parent AGI
+#
+#
+##fig2
+#print('Displaying: % Borrowers without a Pell Grant vs. Mean Balance')
+#fig1.scatter(centers[:, 0], centers[:, 1],zorder=1, c='black', s=100,alpha=0.85)
+#for i in range(len(Xy)):
+#    fig1.plot(Xy[i][0],Xy[i][1],
+#                           colors[labels[i]],
+#                           markersize=10)
+#    
+#fig1.set_title('% Borrowers without a Pell Grant vs. Mean Balance')
+#fig1.set_xlabel('% Borrowers without a Pell Grant')
+#fig1.set_ylabel('Mean Balance')
+#fig1.show()
 
 
 
